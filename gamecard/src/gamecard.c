b0430ce578f88812310cfb9afd20dcb99ee732d4
carolinaguercio <caroguercio@gmail.com>
2020-06-16 20:37:10 -0300

    Creo proceso gamecard

---
+++ gamecard/src/gamecard.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : gamecard.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include "gamecard.h"
+
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ gamecard/src/gamecard.h
@@ -0,0 +1,16 @@
+/*
+ * gamecard.h
+ *
+ *  Created on: 16 jun. 2020
+ *      Author: utnso
+ */
+
+#ifndef GAMECARD_H_
+#define GAMECARD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#endif /* GAMECARD_H_ */
