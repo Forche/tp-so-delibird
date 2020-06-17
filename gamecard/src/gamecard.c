c1af95149be562f3313510d109abfed32705128f
carolinaguercio <caroguercio@gmail.com>
2020-06-16 21:59:21 -0300

    gamecard configs

--- gamecard/src/gamecard.c
+++ gamecard/src/gamecard.c
@@ -12,6 +12,34 @@
 
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	read_config();
+
+	init_loggers();
+
+	shutdown_gamecard();
+
 	return EXIT_SUCCESS;
+}
+
+void read_config() {
+	config = config_create("gamecard.config");
+
+	TIEMPO_DE_REINTENTO_CONEXION = config_get_int_value(config, "TIEMPO_DE_REINTENTO_CONEXION");
+	TIEMPO_DE_REINTENTO_OPERACION = config_get_int_value(config, "TIEMPO_DE_REINTENTO_OPERACION");
+	TIEMPO_RETARDO_OPERACION = config_get_int_value(config, "TIEMPO_RETARDO_OPERACION");
+	PUNTO_MONTAJE_TALLGRASS = config_get_string_value(config, "PUNTO_MONTAJE_TALLGRASS");
+	IP_BROKER = config_get_string_value(config, "IP_BROKER");
+	PUERTO_BROKER = config_get_string_value(config, "PUERTO_BROKER");
+
+}
+
+void init_loggers() {
+
+	logger = log_create("gamecard.log", "gamecard", true, LOG_LEVEL_INFO);
+
+}
+
+void shutdown_gamecard() {
+	log_destroy(logger);
 }
--- gamecard/src/gamecard.h
+++ gamecard/src/gamecard.h
@@ -10,7 +10,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/config.h>
+#include <commons/log.h>
 
 
+// ******* DEFINICION DE VARIABLES A UTILIZAR ******* //
+
+t_log* logger;
+
+t_config* config;
+int TIEMPO_DE_REINTENTO_CONEXION;
+int TIEMPO_DE_REINTENTO_OPERACION;
+int TIEMPO_RETARDO_OPERACION;
+char* PUNTO_MONTAJE_TALLGRASS;
+char* IP_BROKER;
+char* PUERTO_BROKER;
+
+
+// ******* DEFINICION DE FUNCIONES A UTILIZAR ******* //
+void read_config();
+void init_loggers();
+
+void shutdown_gamecard();
 
 #endif /* GAMECARD_H_ */
