626e9da2bb29bfda4beabd4d65472ddc53a8c9c1
Gustavo Forcherio <gustavoforche@gmail.com>
2020-07-23 22:03:00 -0300

    Archivo de deploy

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+cd
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+cd so-commons-library/
+sudo make install
+cd
+
+git clone https://github.com/sisoputnfrba/tp-2020-1c-Operavirus.git
+cd /home/utnso/tp-2020-1c-Operavirus/operavirus-commons/Debug
+make clean
+make all
+cp liboperavirus-commons.so /usr/lib/
+cd
+
+cd /home/utnso/tp-2020-1c-Operavirus/broker/Debug
+make clean
+make all
+cd
+
+cd /home/utnso/tp-2020-1c-Operavirus/gameboy/Debug
+make clean
+make all
+cd
+
+cd /home/utnso/tp-2020-1c-Operavirus/team/Debug
+make clean
+make all
+cd
+
+cd /home/utnso/tp-2020-1c-Operavirus/gamecard/Debug
+make clean
+make all
+cd
+
+
+
+
