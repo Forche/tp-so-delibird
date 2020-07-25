3093515a260abce6039ebab53cf71177e3815ad6
Gustavo Forcherio <gustavoforche@gmail.com>
2020-07-24 21:39:57 -0300

    Script clean proyectos

---
+++ clean.sh
@@ -0,0 +1,20 @@
+cd /home/utnso/tp-2020-1c-Operavirus/operavirus-commons/Debug
+make clean
+cd
+
+cd /home/utnso/tp-2020-1c-Operavirus/broker/Debug
+make clean
+cd
+
+cd /home/utnso/tp-2020-1c-Operavirus/gameboy/Debug
+make clean
+cd
+
+cd /home/utnso/tp-2020-1c-Operavirus/team/Debug
+make clean
+cd
+
+cd /home/utnso/tp-2020-1c-Operavirus/gamecard/Debug
+make clean
+cd
+
